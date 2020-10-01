69a92d2a8a6e57d03392ac3ea2ca2d76d21f7147
Daniel <dgotlieb8@gmail.com>
2020-10-01 21:09:31 +0300

    Initial commit

---
+++ api/server.py
@@ -0,0 +1,26 @@
+import logging
+
+from flask import Flask
+from prometheus_flask_exporter import PrometheusMetrics
+
+logging.basicConfig(level=logging.INFO)
+logging.info("Setting LOGLEVEL to INFO")
+
+api = Flask(__name__)
+metrics = PrometheusMetrics(api)
+
+metrics.info("app_info", "App Info, this can be anything you want", version="1.0.0")
+
+
+@api.route("/say-hello/")
+def hello():
+    return {"message": "hello"}
+
+
+@api.route("/error")
+def error():
+    i = 1/0
+    return str(i), 500
+
+if __name__ == '__main__':
+    api.run('0.0.0.0', port=5000)
